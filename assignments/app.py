d71a03f5d07c7c8a891cc0d3d5bd7f67ea0c99e1
Ubuntu <ubuntu@ip-172-31-17-169.us-west-1.compute.internal>
2019-04-13 05:12:11 +0000

    Adding files for assignemnt1

---
+++ assignments/app.py
@@ -0,0 +1,8 @@
+# app.py
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello, Welcome to the webserver of team ELFS'
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')

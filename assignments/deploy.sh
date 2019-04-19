51bfdc477d90706c055c6543863d634091239871
Partha S. Ghosh <psgliux@gmail.com>
2019-04-18 21:47:41 -0700

    - mongodb persistant - -s option in deploy.sh to stop the containers
    Signed-off-by: Partha S. Ghosh <psgliux@gmail.com>

--- assignments/deploy.sh
+++ assignments/deploy.sh
@@ -13,6 +13,14 @@ DCRMONGODB=elfs-mongodb
 DCRNGNXNAME=nginx
 DCRFLASKNAME=flask
 DCRMONGODBNAME=mongodb
+MONGODBPERSIST=/tmp/mongodb #/var/www/mongodb
+
+function create_mongo_db_dir() {
+    if [ ! -d $MONGODBPERSIST ]; then
+        echo "creating persistent mongodb directory"
+        mkdir -p $MONGODBPERSIST
+    fi
+}
 
 function build_elfs_app() {
     echo "Building ...."
@@ -24,20 +32,29 @@ function build_elfs_app() {
 function deploy_elfs_app() {
     echo "Deploying Team Elf's webserver and application..."
 
+    #create_mongo_db_dir
 	docker network create $DCRNET
 	docker run -d --name $DCRFLASKNAME --net $DCRNET -v "./app" $DCRFLASK
 	docker run -d --name $DCRNGNXNAME --net $DCRNET -p "80:80" $DCRNGNX
-	docker run -d --name $DCRMONGODBNAME -d -v /tmp/mongodb:/data/db -p 27017:27017 $DCRMONGODB
+	docker run -d --name $DCRMONGODBNAME -d -v $MONGODBPERSIST:/data/db -p 27017:27017 $DCRMONGODB
+    docker ps
 
 }
 
 function clean_elfs_app() {
     echo "Cleaning Team Elfs webserve and applicatio..."
-    docker kill $DCRFLASKNAME $DCRNGNXNAME
-    docker rm $DCRFLASKNAME $DCRNGNXNAME
+    docker kill $DCRFLASKNAME $DCRNGNXNAME $DCRMONGODBNAME
+    docker rm $DCRFLASKNAME $DCRNGNXNAME $DCRMONGODBNAME
     docker network rm $DCRNET
-    docker rmi $DCRNGNX $DCRFLASK
-    docker rm $DCRMONGODBNAME $DCRMONGODBNAME
+    docker rmi $DCRNGNX $DCRFLASK $DCRMONGODB
+}
+
+function stop_elfs_app() {
+    echo "Stopping Team Elfs webserve and applicatio..."
+    docker kill $DCRFLASKNAME $DCRNGNXNAME $DCRMONGODBNAME
+    docker rm $DCRFLASKNAME $DCRNGNXNAME $DCRMONGODBNAME
+    docker network rm $DCRNET
+    docker ps
 }
 
 # Usage info
@@ -47,21 +64,22 @@ Usage: ${0##*/} [-b] [-c] [-i] [-h]
 This script is for build and deploy webserver and application
 
     -b          build the containers needed for deployment
-    -c          clean the containers
+    -c          clean the containers, (stop and clean the container images)
     -i          start the containers  webserver, uwsgi, app server and mongodb
+    -s          stop the running containers 
     -h          help
 EOF
     exit 1;
 }
 
 function main() {
-    
+
     if [[ $# -eq 0 ]] ; then
         usage
         exit 0
     fi
-    
-    while getopts "bcih" o; do
+
+    while getopts "bcihs" o; do
         case "${o}" in
         b)
             build_elfs_app
@@ -71,6 +89,9 @@ function main() {
             ;;
         i)
             deploy_elfs_app
+            ;;
+        s)
+            stop_elfs_app
             ;;
         h)
             usage

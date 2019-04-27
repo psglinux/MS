0fa27b2761ae8e08dcf9d5aebbdc142a4ec3c5b8
Chaitanya Lala <chaitanya.lala@gmail.com>
2019-04-27 14:14:59 -0700

    Added Unit test for order creation

--- assignments/addcustapi_ut.py
+++ assignments/addcustapi_ut.py
@@ -3,20 +3,26 @@ import mongomock
 import json
 import addcustapi
 
+book1 = 'A test book'
+book2 = 'Another test book'
+book3 = 'A rare book'
+cust_email = 'cl@gm.com'
+
 class AddCustTests(unittest.TestCase):
     def setUp(self):
         self.db = mongomock.MongoClient()['testdb']
 
-        self.db.book.insert_one({'_id': '1', 'title': 'A test book', 'cost': '1'})
-        self.db.book.insert_one({'_id': '2', 'title': 'Another test book', 'cost': '4' })
-        self.db.book.insert_one({'_id': '3', 'title': 'A rare book', 'cost': '40'})
-        self.db.inventory.insert_one({'_id': '1', 'id': '1', 'quantity': 5})
-        self.db.inventory.insert_one({'_id': '2', 'id': '2', 'quantity': 3})
-        self.db.inventory.insert_one({'_id': '3', 'id': '4', 'quantity': 1})
-
-        cust = addcustapi.Customer('Clala', 'cl@gm.com', '3131324411', '831001')
+        cust = addcustapi.Customer('Clala', cust_email, '3131324411', '831001')
         ret = addcustapi.add_customer(self.db, cust)
         self.assertEqual(ret, True)
+        # Setup for the second test
+        self.db.books.insert_one({'_id': '1', 'Title': book1, 'Price': '1', 'ISBN-13':'djksdksdksd'})
+        self.db.books.insert_one({'_id': '2', 'Title': book2, 'Price': '4', 'ISBN-13':'fdsffsdfs' })
+        self.db.books.insert_one({'_id': '3', 'Title': book3, 'Price': '40', 'ISBN-13':'fsdjfsdjjsd'})
+        self.db.inventory.insert_one({'_id': '1', 'quantity': 5})
+        self.db.inventory.insert_one({'_id': '2', 'quantity': 3})
+        self.db.inventory.insert_one({'_id': '3', 'quantity': 1})
+
 
     def tearDown(self):
         pass
@@ -27,7 +33,7 @@ class AddCustTests(unittest.TestCase):
         self.assertEqual(ret, True)
 
         # Re-use of email ID - not permitted
-        cust = addcustapi.Customer('Pavan', 'cl@gm.com', '3131311111', '831002')
+        cust = addcustapi.Customer('Pavan', cust_email, '3131311111', '831002')
         ret = addcustapi.add_customer(self.db, cust)
         self.assertEqual(ret, False)
 
@@ -40,7 +46,18 @@ class AddCustTests(unittest.TestCase):
         for customer in self.db.customers.find({}):
             num_cust += 1
         self.assertEqual(num_cust, 3)
-        print("PASS AddCustTests")
+        print("PASS add customer tests")
 
+    def test_customer_order(self):
+        # Send an order for a few books.
+        book_to_qty = {book1 : 3, book2 : 4, book3 : 0} 
+        customer, order = addcustapi.order_books(self.db, cust_email, book_to_qty)
+        print(customer)
+        self.assertEqual(customer['email_address'], cust_email) 
+        self.assertEqual(customer['Name'], 'Clala') 
+        print(order)
+        self.assertEqual(len(order), 1)
+        self.assertEqual(order[0]['Title'], book1)
+        
 if __name__ == '__main__':
     unittest.main()

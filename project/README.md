f0359bb2a390d52d153bffa16f9db488a52cfac1
Partha S. Ghosh <psglinux@gmail.com>
2019-04-27 15:25:15 -0700

    create the framework for the project-app
    TODO :
            - create different container images for project
            - add code to build the database of the home data
    
    Signed-off-by: Partha S. Ghosh <psglinux@gmail.com>

---
+++ project/README.md
@@ -0,0 +1,61 @@
+# Project :
+  Our Project uses the assignment infrastructure code base as the fundamental
+building block for project.
+
+# Architecture
+
+The project seperates the building blocks of the appplication to 3 different
+dockers in deployment.
+    - A Docker for running the Web Server
+    - A Docker for running the application Server
+    - A Docker for running the Database (Mongo DB)
+    - In the Deployment glue these containers using Docker Networking
+
+*NOTE* : There is no assumption that the applicaton would be deployed on only one server
+    
+
+## Why choose this Architecture ?
+The application architecture is based on micro-services. This is a disaggregated
+architecture. Say if the application needs to scale, we can take any of the 
+components and run them in different servers. With very little changes in 
+configuration all the infrastructure can be made to work seamlessly and scale 
+up to serve more users.
+
+
+*TODO:* redo the bottom part of the write up
+
+# Bringing up the uWSGI server and routing the traffic through Nginx
+
+## Manual Steps to create a web application stack using nginx-uWSGI-flask
+
+- Step 1: Create a requirments.txt file with dependencies for python application
+- Step 2: Create an project-app.py file with web application logic
+- Step 3: Create an project-app.ini file with uWSGI operation logic
+- Step 4: Build the flask docker image
+- Step 5: Create an project-app.conf file with configuration for routing traffic to uWSGI
+- Step 6: Build the nginx docker image
+- Step 7: Create the docker compose yaml file
+- Step 8: Run the below command for building and deploying
+    ```
+    docker build -t my-flask -f Dockerfile-flask .
+	docker build -t my-nginx -f Dockerfile-nginx .
+	docker network create my-network
+	docker run -d --name flask --net my-network -v "./project_app" my-flask
+	docker run -d --name nginx --net my-network -p "80:80" my-nginx
+    ```
+
+
+# Assignemnt2: Design and Build an Online Bookstore Enterprise Database
+
+## Manual Steps to bring up the mongodb container
+
+```
+docker build -t my-mongodb -f Dockerfile-mongodb .
+docker run -d --name mongodb  -d -v /tmp/mongodb:/data/db -p 27017:27017 my-mongodb
+```
+
+
+# Build, deploy and Clean
+./deploy-project.sh -h
+
+
---
+++ project/deploy-project.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+#########################################
+# author : Team Elfs
+# script : bash script for build and install of cmpe-272
+#########################################
+
+#!/bin/bash
+
+DCRFLASK=elfs-flask
+DCRNGNX=elfs-nginx
+DCRNET=elfs-network
+DCRMONGODB=elfs-mongodb
+DCRNGNXNAME=nginx
+DCRFLASKNAME=flask
+DCRMONGODBNAME=mongodb
+MONGODBPERSIST=/var/www/project-mongodb
+
+function create_mongo_db_dir() {
+    if [ ! -d $MONGODBPERSIST ]; then
+        echo "creating persistent mongodb directory"
+        mkdir -p $MONGODBPERSIST
+    fi
+}
+
+function build_elfs_app() {
+    echo "Building ...."
+	docker build -t $DCRFLASK -f Dockerfile-project-flask .
+	docker build -t $DCRNGNX -f Dockerfile-project-nginx .
+	docker build -t $DCRMONGODB -f Dockerfile-project-mongodb .
+}
+
+function deploy_elfs_app() {
+    echo "Deploying Team Elf's webserver and application..."
+
+    #create_mongo_db_dir
+	docker network create $DCRNET
+	docker run -d --name $DCRFLASKNAME --net $DCRNET -v "./project-app" $DCRFLASK
+	docker run -d --name $DCRNGNXNAME --net $DCRNET -p "80:80" $DCRNGNX
+	docker run -d --name $DCRMONGODBNAME -d -v $MONGODBPERSIST:/data/db -p 27017:27017 $DCRMONGODB
+    docker ps
+
+}
+
+function clean_elfs_app() {
+    echo "Cleaning Team Elfs webserve and applicatio..."
+    docker kill $DCRFLASKNAME $DCRNGNXNAME $DCRMONGODBNAME
+    docker rm $DCRFLASKNAME $DCRNGNXNAME $DCRMONGODBNAME
+    docker network rm $DCRNET
+    docker rmi $DCRNGNX $DCRFLASK $DCRMONGODB
+}
+
+function stop_elfs_app() {
+    echo "Stopping Team Elfs webserve and applicatio..."
+    docker kill $DCRFLASKNAME $DCRNGNXNAME $DCRMONGODBNAME
+    docker rm $DCRFLASKNAME $DCRNGNXNAME $DCRMONGODBNAME
+    docker network rm $DCRNET
+    docker ps
+}
+
+# Usage info
+usage() {
+cat << EOF
+Usage: ${0##*/} [-b] [-c] [-i] [-h]
+This script is for build and deploy webserver and application
+
+    -b          build the containers needed for deployment
+    -c          clean the containers, (stop and clean the container images)
+    -i          start the containers  webserver, uwsgi, app server and mongodb
+    -s          stop the running containers
+    -h          help
+EOF
+    exit 1;
+}
+
+function main() {
+
+    if [[ $# -eq 0 ]] ; then
+        usage
+        exit 0
+    fi
+
+    while getopts "bcihs" o; do
+        case "${o}" in
+        b)
+            build_elfs_app
+            ;;
+        c)
+            clean_elfs_app
+            ;;
+        i)
+            deploy_elfs_app
+            ;;
+        s)
+            stop_elfs_app
+            ;;
+        h)
+            usage
+            ;;
+        *)
+            usage
+            ;;
+        esac
+    done
+    shift 0
+
+}
+
+main $@
+

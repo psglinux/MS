073fe8a2d2137ee9e219348ad88d546c07f42439
Partha S. Ghosh <psglinux@gmail.com>
2019-05-03 01:58:49 -0700

    fixed the setup needed to indicate unit test
    Signed-off-by: Partha S. Ghosh <psglinux@gmail.com>

--- assignments/bookstore_flask_ut.py
+++ assignments/bookstore_flask_ut.py
@@ -12,6 +12,19 @@ TEST_DB = 'test_database'
 
 class BasicTestCase(unittest.TestCase):
 
+    def setUp(self):
+        """
+        Set up a temp database before each test. since this is not end to
+        end test use mongomock to create a testdb. This DB will have the same
+        schema as that of the original db.
+        """
+        app.testing = True
+        pass
+
+    def tearDown(self):
+        """using mongomock for the ut test. Hence no clean up is needed """
+        pass
+
     def test_index(self):
         """initial test. ensure flask was set up correctly"""
         tester = app.test_client(self)

09aee2812c337b22a62d8207566dcb65d5a80457
Partha S. Ghosh <psglinux@gmail.com>
2020-02-07 22:52:31 -0800

    problem 2 : added verifications
    Signed-off-by: Partha S. Ghosh <psglinux@gmail.com>

--- assignment-2/assignment2.py
+++ assignment-2/assignment2.py
@@ -16,9 +16,10 @@ def find_one_e_or_d(tot, e):
            break
        i += 1
 
-def find_C(M, e, N):
+def find_C(M, e, N, m):
     C = pow(M, e)%N
-    print "Cipher :", C
+    print m, ":", C
+    return C
 
 def find_d_for_e(e, r, tot):
     print "e = ", e
@@ -31,7 +32,7 @@ def problem_1():
     print "problem 1 : ------- starts"
     find_prime_factor(187)
     find_one_e_or_d(160, 107)
-    find_C(8, 107, 187)
+    find_C(8, 107, 187, "Cipher")
     print "problem 1 : ------- ends"
 
 def problem_2():
@@ -39,6 +40,8 @@ def problem_2():
     find_d_for_e(3, 1000, 40)
     find_d_for_e(5, 1000, 40)
     find_d_for_e(7, 1000, 40)
+    c = find_C(8, 3, 55, "Cipher")
+    find_C(c, 67, 55, "Plain")
     print "problem 2 : ------- ends"
 
 problem_1()
